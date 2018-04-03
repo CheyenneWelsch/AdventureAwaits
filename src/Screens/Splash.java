ebeefa1f2e3a01ab35fd7c2913c21ef078ff7619
cheyennewelsch <cheyennewelsch@192.168.0.17>
2018-04-03 13:35:24 -0400

    ??

---
+++ src/Screens/Splash.java
@@ -0,0 +1,35 @@
+package Screens;
+
+import javafx.application.Application;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.animation.FadeTransition;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.layout.VBox;
+import javafx.scene.web.WebView;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.util.Duration;
+import org.w3c.dom.Document;
+
+
+/*
+ * Method 1: initialize the splash screen
+ * Method 2: Display splash screen
+ * Method 3: Update progress bar
+ * Method 4: End splash screen
+ * 
+ */
+public abstract class Splash extends Application {
+}
