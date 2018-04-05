1b6ad3e22bd2f0a98dae60058c4c1e0775e7b743
cwelsch2 <cwelsch2@IST-CS200-10.dmd.gsuad.gsu.edu>
2018-04-05 18:04:20 -0400

    User class

--- src/UML/Users.java
+++ src/UML/Users.java
@@ -1,5 +1,29 @@
 package UML;
 
+import java.util.*;
+
 public class Users {
 
+	protected static void main(String[] args){
+		
+	
+	//Generics shifts the issue from a runtime error to a compile time error
+	//can only pass strings without casting
+	
+	ArrayList<Users> list = new ArrayList<Users>();
+	
+	//behind the scenes of a dynamic array: gives the best performance. 
+	//When it fills, it doubles it, fills existing, and
+	//provides you with the same amount of new spots
+	
+	list.add(new Users());
+	
+	Users u2 = list.get(0);
+	
+	Users u3 = list.get(1);
+	
+	
+	
+	
+	}
 }
