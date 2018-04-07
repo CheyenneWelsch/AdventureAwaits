c8a51c3da007eeed94e6515ed7852430f16268cc
crazymoose55 <crazymoose55@maxs-mbp.attlocal.net>
2018-04-06 20:10:36 -0400

    Added attributes to customer & created flight (along with attributes)

---
+++ src/UML/Flight.java
@@ -0,0 +1,20 @@
+package UML;
+
+import java.util.Date;
+
+public class Flight {
+	private String destinationCity;
+	private String destinationAirport;
+	private String arrivalCity;
+	private String arrivalAirport;
+	
+	
+	//LocalDate.of(2018, 08, 21) or LocalDate.parse("2018-08-21") represents a specific day, month, year
+	//LocalTime.of(6, 30) or LocalTime.parse("6:30") specific time
+	private Date departureDate;
+	private Date departureTime;
+	
+	
+	
+	
+}
